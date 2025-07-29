0220da59907a82995d8da18dd82e4c0647611c7a
partkyle <partkyle@gmail.com>
2025-07-28 22:43:15 -0700

    multiple changes:
    - create scene from hand and migrate sketchfab over to that
    - add melee attack
    - add sky shader and generate stars

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -3,6 +3,8 @@ class_name Player
 
 const EPSILON := 0.005
 
+@onready var ray_cast_3d: RayCast3D = $RayCast3D
+
 @export var acceleration_intensity := 5.0
 @export var deceleration_intensity := 5.0
 @export var booster_speed := 1.0
@@ -10,7 +12,14 @@ const EPSILON := 0.005
 @export var stick_rotation_speed := rotation_speed * 2.0
 
 
-func _physics_process(delta: float) -> void:	
+func handle_melee():
+	if Input.is_action_just_pressed("melee"):
+		if ray_cast_3d.is_colliding() and ray_cast_3d.get_collider().get_parent() is Planet:
+			global_position = lerp(global_position, ray_cast_3d.get_collision_point(), 0.9)
+
+func _physics_process(delta: float) -> void:
+	handle_melee()
+
 	if Input.is_action_pressed("shoot"):
 		Laser.create(get_tree().root, self.global_position, self.velocity, self.basis)
 	
