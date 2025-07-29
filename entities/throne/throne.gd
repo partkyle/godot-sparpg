fbbace70da8fd9e79c53b846b3e4ed37465e5272
partkyle <partkyle@gmail.com>
2025-07-28 23:11:33 -0700

    add throne character and shift to 3rd person

---
+++ entities/throne/throne.gd
@@ -0,0 +1,20 @@
+extends Node3D
+class_name Throne
+
+@onready var rings : Array[Node3D] = [
+	$Ring1,
+	$Ring2,
+	$Ring3,
+	$Ring4,
+	$Ring5,
+	$Ring6,
+]
+
+func _ready() -> void:
+	print(rings)
+
+func _process(delta: float):
+	var i = 1
+	for r in rings:
+		i += 1
+		r.rotate_x(i * delta)
