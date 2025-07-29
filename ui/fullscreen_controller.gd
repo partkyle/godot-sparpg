925678c8d0a3c69898b9f4c590e2e0e697885212
partkyle <partkyle@Mac.attlocal.net>
2025-07-28 23:32:36 -0700

    add fullscreen switch

---
+++ ui/fullscreen_controller.gd
@@ -0,0 +1,11 @@
+extends Node
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		if event.pressed and event.keycode == KEY_0:
+			if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
+				print('switching to windowedb')
+				DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+			else:
+				print('switching to fullscreen')
+				DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
