ad3059c3308513a84eb83f524dd96c8004278410
partkyle <partkyle@gmail.com>
2025-07-29 22:36:26 -0700

    goofy planet behavior

--- entities/planet/planet.gd
+++ entities/planet/planet.gd
@@ -3,13 +3,35 @@
 extends Node3D
 class_name Planet
 
+@export var spd := 1.0
+
 @export var rotation_speed := 1.0
 @onready var mesh_instance_3d: MeshInstance3D = $"MeshInstance3D"
 
 @export var player: Player
+
+var t : SceneTreeTimer
+
+var velocity : Vector3
+
+func _ready() -> void:
+	t = get_tree().create_timer(10.0)
+	t.timeout.connect(reset_speed)
+
+func reset_speed() -> void:
+	velocity = Vector3.ZERO
 
 func _process(delta: float) -> void:
 	mesh_instance_3d.rotate_y(rotation_speed * delta)
 	
 	if player:
 		look_at(player.global_position)
+
+func _physics_process(delta: float) -> void:
+	if player:
+		var direction = player.global_position - global_position
+		if direction.length_squared() > 0:
+			velocity += direction.normalized() * spd * delta
+	
+	if velocity:
+		global_position += velocity * delta
